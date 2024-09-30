da34f6c7cd31f3c6524805eaf6f7ff1f6f943de2
Yampi <51342050+cdryampi@users.noreply.github.com>
2024-10-01 01:15:37 +0200

    Update settings.py

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'webEmpresa',
         'USER': 'postgres',
         'PASSWORD': 'thos',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 
